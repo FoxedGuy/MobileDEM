1ea0753eb4a9e0038bf7c666c4e25451bfe94854
Damian.G <69583411+FoxedGuy@users.noreply.github.com>
2023-07-08 18:04:28 +0200

    delete main and unused function from models.py

--- models.py
+++ models.py
@@ -157,35 +157,6 @@ def Generator(input_channels=INPUT_SHAPE, output_channels=OUTPUT_SHAPE):
 
     return tf.keras.Model(inputs=inputs, outputs=x)
 
-
-def Discriminator(input_channels=INPUT_SHAPE, output_channels=OUTPUT_SHAPE):
-    initializer = tf.random_normal_initializer(0., 0.02)
-
-    inp = tf.keras.layers.Input(shape=[256, 256, input_channels], name='input_image')
-    tar = tf.keras.layers.Input(shape=[256, 256, output_channels], name='target_image')
-
-    x = tf.keras.layers.concatenate([inp, tar])  # (bs, 256, 256, channels*2)
-
-    down1 = downsample(64, 4, False)(x)  # (bs, 128, 128, 64)
-    down2 = downsample(128, 4)(down1)  # (bs, 64, 64, 128)
-    down3 = downsample(256, 4)(down2)  # (bs, 32, 32, 256)
-
-    zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)  # (bs, 34, 34, 256)
-    conv = tf.keras.layers.Conv2D(512, 4, strides=1,
-                                  kernel_initializer=initializer,
-                                  use_bias=False)(zero_pad1)  # (bs, 31, 31, 512)
-
-    batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
-
-    leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
-
-    zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (bs, 33, 33, 512)
-
-    last = tf.keras.layers.Conv2D(1, 4, strides=1,
-                                  kernel_initializer=initializer)(zero_pad2)  # (bs, 30, 30, 1)
-
-    return tf.keras.Model(inputs=[inp, tar], outputs=last)
-
 def create_discriminator(input_channels=INPUT_SHAPE, output_channels=OUTPUT_SHAPE):
     initializer = tf.random_normal_initializer(0., 0.02)
 
@@ -213,14 +184,6 @@ def create_discriminator(input_channels=INPUT_SHAPE, output_channels=OUTPUT_SHAP
                                 kernel_initializer=initializer)(zero_pad2) # (bs, 30, 30, 1)
 
     return tf.keras.Model(inputs=[inp, tar], outputs=last)
-
-
-def main():
-    gen_small = create_generator_small()
-    gen_eff = create_generator_efficient()
-    gen_pix = Generator()
-
-    print(gen_small.summary())
 
 if __name__ == "__main__":
     main()
